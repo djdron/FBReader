7a64c639ac9b4c43b89d0e99c289cd3cc59f15ae
djdron <djdron@gmail.com>
2021-11-03 01:26:54 +0200

    using ":" as path delimiter (compatible with std desktop config)

--- zlibrary/core/src/win32/library/ZLibrary.cpp
+++ zlibrary/core/src/win32/library/ZLibrary.cpp
@@ -23,7 +23,7 @@
 #include "../../unix/library/ZLibraryImplementation.h"
 
 const std::string ZLibrary::FileNameDelimiter("/");
-const std::string ZLibrary::PathDelimiter(";");
+const std::string ZLibrary::PathDelimiter(":");
 const std::string ZLibrary::EndOfLine("\r\n");
 
 void ZLibrary::initLocale() {
